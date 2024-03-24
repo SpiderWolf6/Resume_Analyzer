76c94c275ce381a2bde30be536ea4a6ceb5c2ea3
Soham Mukherjee <100542752+SpiderWolf6@users.noreply.github.com>
2024-03-23 20:07:36 -0400

    Add files via upload

---
+++ Courses.py
@@ -0,0 +1,141 @@
+ds_course = [['Machine Learning Crash Course by Google [Free]', 'https://developers.google.com/machine-learning/crash-course'],
+             ['Machine Learning A-Z by Udemy','https://www.udemy.com/course/machinelearning/'],
+             ['Machine Learning by Andrew NG','https://www.coursera.org/learn/machine-learning'],
+             ['Data Scientist Master Program of Simplilearn (IBM)','https://www.simplilearn.com/big-data-and-analytics/senior-data-scientist-masters-program-training'],
+             ['Data Science Foundations: Fundamentals by LinkedIn','https://www.linkedin.com/learning/data-science-foundations-fundamentals-5'],
+             ['Data Scientist with Python','https://www.datacamp.com/tracks/data-scientist-with-python'],
+             ['Programming for Data Science with Python','https://www.udacity.com/course/programming-for-data-science-nanodegree--nd104'],
+             ['Programming for Data Science with R','https://www.udacity.com/course/programming-for-data-science-nanodegree-with-R--nd118'],
+             ['Introduction to Data Science','https://www.udacity.com/course/introduction-to-data-science--cd0017'],
+             ['Intro to Machine Learning with TensorFlow','https://www.udacity.com/course/intro-to-machine-learning-with-tensorflow-nanodegree--nd230']]
+
+web_course = [['Django Crash course [Free]','https://youtu.be/e1IyzVyrLSU'],
+              ['Python and Django Full Stack Web Developer Bootcamp','https://www.udemy.com/course/python-and-django-full-stack-web-developer-bootcamp'],
+              ['React Crash Course [Free]','https://youtu.be/Dorf8i6lCuk'],
+              ['ReactJS Project Development Training','https://www.dotnettricks.com/training/masters-program/reactjs-certification-training'],
+              ['Full Stack Web Developer - MEAN Stack','https://www.simplilearn.com/full-stack-web-developer-mean-stack-certification-training'],
+              ['Node.js and Express.js [Free]','https://youtu.be/Oe421EPjeBE'],
+              ['Flask: Develop Web Applications in Python','https://www.educative.io/courses/flask-develop-web-applications-in-python'],
+              ['Full Stack Web Developer by Udacity','https://www.udacity.com/course/full-stack-web-developer-nanodegree--nd0044'],
+              ['Front End Web Developer by Udacity','https://www.udacity.com/course/front-end-web-developer-nanodegree--nd0011'],
+              ['Become a React Developer by Udacity','https://www.udacity.com/course/react-nanodegree--nd019']]
+
+android_course = [['Android Development for Beginners [Free]','https://youtu.be/fis26HvvDII'],
+                  ['Android App Development Specialization','https://www.coursera.org/specializations/android-app-development'],
+                  ['Associate Android Developer Certification','https://grow.google/androiddev/#?modal_active=none'],
+                  ['Become an Android Kotlin Developer by Udacity','https://www.udacity.com/course/android-kotlin-developer-nanodegree--nd940'],
+                  ['Android Basics by Google','https://www.udacity.com/course/android-basics-nanodegree-by-google--nd803'],
+                  ['The Complete Android Developer Course','https://www.udemy.com/course/complete-android-n-developer-course/'],
+                  ['Building an Android App with Architecture Components','https://www.linkedin.com/learning/building-an-android-app-with-architecture-components'],
+                  ['Android App Development Masterclass using Kotlin','https://www.udemy.com/course/android-oreo-kotlin-app-masterclass/'],
+                  ['Flutter & Dart - The Complete Flutter App Development Course','https://www.udemy.com/course/flutter-dart-the-complete-flutter-app-development-course/'],
+                  ['Flutter App Development Course [Free]','https://youtu.be/rZLR5olMR64']]
+
+ios_course = [['IOS App Development by LinkedIn','https://www.linkedin.com/learning/subscription/topics/ios'],
+              ['iOS & Swift - The Complete iOS App Development Bootcamp','https://www.udemy.com/course/ios-13-app-development-bootcamp/'],
+              ['Become an iOS Developer','https://www.udacity.com/course/ios-developer-nanodegree--nd003'],
+              ['iOS App Development with Swift Specialization','https://www.coursera.org/specializations/app-development'],
+              ['Mobile App Development with Swift','https://www.edx.org/professional-certificate/curtinx-mobile-app-development-with-swift'],
+              ['Swift Course by LinkedIn','https://www.linkedin.com/learning/subscription/topics/swift-2'],
+              ['Objective-C Crash Course for Swift Developers','https://www.udemy.com/course/objectivec/'],
+              ['Learn Swift by Codecademy','https://www.codecademy.com/learn/learn-swift'],
+              ['Swift Tutorial - Full Course for Beginners [Free]','https://youtu.be/comQ1-x2a1Q'],
+              ['Learn Swift Fast - [Free]','https://youtu.be/FcsY1YPBwzQ']]
+
+uiux_course = [['Google UX Design Professional Certificate','https://www.coursera.org/professional-certificates/google-ux-design'],
+               ['UI / UX Design Specialization','https://www.coursera.org/specializations/ui-ux-design'],
+               ['The Complete App Design Course - UX, UI and Design Thinking','https://www.udemy.com/course/the-complete-app-design-course-ux-and-ui-design/'],
+               ['UX & Web Design Master Course: Strategy, Design, Development','https://www.udemy.com/course/ux-web-design-master-course-strategy-design-development/'],
+               ['The Complete App Design Course - UX, UI and Design Thinking','https://www.udemy.com/course/the-complete-app-design-course-ux-and-ui-design/'],
+               ['DESIGN RULES: Principles + Practices for Great UI Design','https://www.udemy.com/course/design-rules/'],
+               ['Become a UX Designer by Udacity','https://www.udacity.com/course/ux-designer-nanodegree--nd578'],
+               ['Adobe XD Tutorial: User Experience Design Course [Free]','https://youtu.be/68w2VwalD5w'],
+               ['Adobe XD for Beginners [Free]','https://youtu.be/WEljsc2jorI'],
+               ['Adobe XD in Simple Way','https://learnux.io/course/adobe-xd']]
+
+medicine_course = [['Medical Terminology Certificate by Rice University', 'https://www.coursera.org/specializations/medicalterminology'],
+                   ['Understanding the Brain: The Neurobiology of Everyday Life', 'https://www.coursera.org/learn/neurobiology'],
+                   ['Learn to Speak "Medicine" and Start Your Healthcare Career', 'https://www.udemy.com/course/learning-medical-terminology/'],
+                   ['Drug Commercialization', 'https://www.coursera.org/learn/drug-commercialization'],
+                   ['Herbalism: Introduction and Medicine Making Certificate', 'https://www.udemy.com/course/herbalism-medicine-making/'],
+                   ['Big Data, Genes, and Medicine by SUNY', 'https://www.coursera.org/learn/data-genes-medicine'],
+                   ['Immunology: The Immune System and its Failure', 'https://www.coursera.org/specializations/immunology-the-immune-system-and-its-failures'],
+                   ['Analytics in Healthcare: the Basics, Plain and Simple', 'https://www.udemy.com/course/h16-advanced-analytics-in-healthcare-plain-simple/'],
+                   ['The Skin, Bones, and Muscles', 'https://www.udemy.com/course/h16-advanced-analytics-in-healthcare-plain-simple/'],
+                   ['Medical Lectures by MIT', 'https://www.youtube.com/watch?v=b_lgH_ZnCmg&list=PLgfyCCmwycpJ5FJ3bmMDQvSZPjHOawrst']]
+
+education_course = [['Innovative Teaching with ChatGPT', 'https://www.coursera.org/learn/chatgpt-innovative-teaching'],
+                    ['Diversity and Inclusion in Education', 'https://www.coursera.org/learn/diversity-and-inclusion-education'],
+                    ['Educational Psychology and Special Education (Certificate)', 'https://www.udemy.com/course/educational-psychology-special-education/'],
+                    ['Complete Guide to Early Childhood Care and Education (ECCE)', 'https://www.udemy.com/course/complete-guide-to-early-childhood-care-educationecce/'],
+                    ['The Complete Early Childhood Education (Lower Kindergarten)', 'https://www.udemy.com/course/the-complete-early-childhood-education-lower-kindergarten/'],
+                    ['Learning, Knowledge, and Human Development', 'https://www.coursera.org/learn/learning-knowledge-human-development'],
+                    ['Introduction to EdTech', 'https://www.coursera.org/learn/introduction-to-edtech'],
+                    ['How We Teach: MIT 5.95J Teaching College-Level Science and Engineering', 'https://www.youtube.com/watch?v=Zm8uMV5aMdw&list=PLUl4u3cNGP60bTiL2hg0h_akgz8ywXkCX&index=1'],
+                    ['Academic Skills for University Success', 'https://www.coursera.org/specializations/academic-skills'],
+                    ['Special Education: Everything you Need to Know', 'https://www.youtube.com/watch?v=H90Po8tHbOU&pp=ygURZWR1Y2F0aW9uIGNvdXJzZXM%3D']]
+
+law_course = [["A Law Student's Toolkit by Yale", 'https://www.youtube.com/watch?v=Kj4TvL5pIc8&list=PLh9mgdi4rNeyfbeQuCxF2nGCMmCcJPoIU'],
+              ['International Law Lectures', 'https://www.youtube.com/watch?v=17dbppFxbCs&list=PLj3bSusRimZNEIPYNl_7nk2cdoqa48WmY'],
+              ['Harvard Jurisprudence Lectures', 'https://www.youtube.com/watch?v=kBdfcR-8hEY&list=PL66DFF275B46BEF40'],
+              ['Criminal Law Crash Course: Law School and Bar Exam Prep', 'https://www.youtube.com/watch?v=qpthxkNQmbc&list=PL0JgU9zLKXAFQ-BFQ_fB0FmYuk9WPbYbg'],
+              ['Constitutional Law', 'https://www.youtube.com/watch?v=P0Kzx9hZALw&list=PLMIM2V8Vm4Yp4HtTIgJwg1d9nzMgX1lQz'],
+              ['Diversity and Legal History: A Harvard Law School Lecture Series', 'https://www.youtube.com/watch?v=HqkMmJ_dkBQ&list=PL2q2U2nTrWq0KixVOa11hBv8wOssEtgLu'],
+              ['An Introduction to American Law by UPenn', 'https://www.coursera.org/learn/american-law'],
+              ['Criminal Law - Law School for Teens', 'https://www.udemy.com/course/law-school-for-teens-criminal-law/'],
+              ['Corporate and Business Law', 'https://www.udemy.com/course/corporate-and-business-law/'],
+              ['Company Law: A Comprehensive Summary', 'https://www.udemy.com/course/learn-company-law/']]
+
+finance_course = [['Financial Markets', 'https://www.coursera.org/learn/financial-markets-global'],
+                  ['Introduction to Corporate Finance', 'https://www.coursera.org/learn/financial-markets-global'],
+                  ['Introduction to Finance, Accounting, Modeling, and Visualization', 'https://www.udemy.com/course/introduction-to-accounting-finance-modeling-valuation-by-chris-haroun/'],
+                  ['The Complete Financial Analyst Training and Investing Course', 'https://www.udemy.com/course/the-complete-financial-analyst-training-and-investing-course/'],
+                  ['MIT 15.401 Finance Theory I', 'https://www.youtube.com/watch?v=HdHlfiOAJyE&list=PLUl4u3cNGP63B2lDhyKOsImI7FjCf6eDW'],
+                  ['ECO 305: Money and Banking', 'https://www.youtube.com/watch?v=etsCGT542AI&list=PLdLiRaajwSXSO6FD9_oQUkjAgQoRHDs8d'],
+                  ['Algorithmic Trading - Machine Learning and Quant Strategies Course with Python', 'https://www.youtube.com/watch?v=9Y3yaoi9rUQ&pp=ygUuZmluYW5jZSBhbmQgZWNvbm9taWNzIGxlY3R1cmUgY291cnNlIHBsYXlsaXN0cw%3D%3D'],
+                  ['MIT 14.01 Principles of Microeconomics (Fall 2018)', 'https://www.youtube.com/watch?v=_OkTw766oCs&list=PLUl4u3cNGP62oJSoqb4Rf-vZMGUBe59G-'],
+                  ['Financial Theory with Professor John Geanakoplos from Yale', 'https://www.youtube.com/watch?v=vTs2IQ8OefQ&list=PLEDC55106E0BA18FC'],
+                  ['ECO 155: Principles of Macroeconomics', 'https://www.youtube.com/watch?v=FKRMXjr3Mko&list=PLJpqFJdf_oz0Slj_-v2QnTpnuvfKeA3Kt']]
+
+business_course = [['International Business', 'https://www.youtube.com/watch?v=fU7xJ2AYM3w&list=PLesgViD0jhW_pW9WVtA4oe4w3TRVecbEQ'],
+                   ['BUS-10 Intro to Business Lectures', 'https://www.youtube.com/watch?v=Su9rkArKEfM&list=PLt37cZnkjmJy1bjifLOkDqsnMQwSH95OX'],
+                   ['Business Entreprenuership - Crash Course', 'https://www.youtube.com/watch?v=YHBVjv4MYXE&list=PL8dPuuaLjXtNamNKW5qlS-nKgA0on7Qze'],
+                   ['Business Management Course 101', 'https://www.youtube.com/watch?v=T3l51Psce3c&list=PL1O57nCUQ-e-OVRFdIB-Gu1U91yH7egmm'],
+                   ['Business Foundations by UPenn', 'https://www.coursera.org/specializations/wharton-business-foundations'],
+                   ['Successful Negotiation: Essential Strategies and Skills', 'https://www.coursera.org/learn/negotiation-skills'],
+                   ['MBA in a Box: Business Lessons from a CEO', 'https://www.udemy.com/course/mba-in-a-box-business-lessons-from-a-ceo/'],
+                   ['Intro to Entreprenuership', 'https://www.udemy.com/course/intro-to-entrepreneurship-get-started-as-an-entrepreneur/'],
+                   ['Diploma Course in Business Management and Entreprenuership', 'https://www.udemy.com/course/diploma-in-management-and-entrepreneurship/'],
+                   ['Business Fundamentals: Corporate Strategy', 'https://www.udemy.com/course/business-fundamentals-corporate-strategy/']]
+
+hr_course = [['Customer Service, Customer Support, and Customer Experience', 'https://www.udemy.com/course/how-to-find-your-voice-the-resonates-with-your-customers/'],
+             ['Market Research and Consumer Behavior', 'https://www.coursera.org/learn/market-research'],
+             ['Customer Service: Soft Skills Fundamentals', 'https://www.udemy.com/course/customer-service-soft-skills-fundamentals/'],
+             ['Retail Customer Service', 'https://www.coursera.org/learn/retail-customer-service'],
+             ['Retail Management Certification Program', 'https://www.udemy.com/course/retail-management-program-from-retail-expert/'],
+             ['Administrative Human Resources (HR) for Beginners', 'https://www.udemy.com/course/administrative-human-resources-for-beginners/'],
+             ['Human Resources Analytics', 'https://www.coursera.org/learn/human-resources-analytics'],
+             ['Customer Service', 'https://www.youtube.com/watch?v=a2ytQ2jLFYY&list=PLWPirh4EWFpEnY0b4Bc_YPWDb3seX4g2o'],
+             ['Retail Management', 'https://www.youtube.com/watch?v=5iRDd-f1nmg&list=PLWPirh4EWFpEv7x2CU-9jcXlIvBuSx7oF'],
+             ['Customer Service Training Videos', 'https://www.youtube.com/watch?v=S_e1j54TRns&list=PLszYngDGf2tmBSl-unLj0y6hmgAsa3l-E']]
+
+media_course = [['Beginners Guide to Graphic Design', 'https://www.youtube.com/watch?v=WONZVnlam6U&list=PLYfCBK8IplO4E2sXtdKMVpKJZRBEoMvpn'],
+                ['Graphic Design Full Course', 'https://www.youtube.com/watch?v=e_dv7GBHka8&pp=ygUlbWVkaWEgZ3JhcGhpYyBkZXNpZ24gY291cnNlcyBwbGF5bGlzdA%3D%3D'],
+                ['Branding: The Creative Journey', 'https://www.coursera.org/specializations/branding-the-creative-journey'],
+                ['Graphic Design Masterclass', 'https://www.udemy.com/course/graphic-design-masterclass-everything-you-need-to-know/'],
+                ['Digital Design Masterclass for Graphic Designers', 'https://www.udemy.com/course/digital-design-masterclass-for-graphic-designers/'],
+                ['Graphic Design Mastery: The Full Branding and Design Process', 'https://www.udemy.com/course/graphic-design-mastery-the-full-branding-design-process/'],
+                ['The Complete Graphic Design Theory for Beginners Course', 'https://www.udemy.com/course/graphic-design-theory-for-beginners-course/'],
+                ['AI Graphic Design, Pro Advertising + Photoshop, Illustrator', 'https://www.udemy.com/course/photoshop-cc-masterclass-be-a-creative-professional/'],
+                ['Graphic Design Masterclass Intermediate: The NEXT Level', 'https://www.udemy.com/course/graphic-design-masterclass-the-next-level/'],
+                ['Graphic Design Theory', 'https://www.youtube.com/watch?v=rfmJI8BqxC8&list=PL0EqspLrOAxRX_IeXFX1jcaqHKCc45IJx']]
+
+resume_videos = ['https://youtu.be/y8YH0Qbu5h4','https://youtu.be/J-4Fv8nq1iA',
+                 'https://youtu.be/yp693O87GmM','https://youtu.be/UeMmCex9uTU',
+                 'https://youtu.be/dQ7Q8ZdnuN0','https://youtu.be/HQqqQx5BCFY',
+                 'https://youtu.be/CLUsplI4xMU','https://youtu.be/pbczsLkv7Cc']
+
+interview_videos = ['https://youtu.be/Ji46s5BHdr0','https://youtu.be/seVxXHi2YMs',
+                    'https://youtu.be/9FgfsLa_SmY','https://youtu.be/2HQmjLu-6RQ',
+                    'https://youtu.be/DQd_AlIvHUw','https://youtu.be/oVVdezJ0e7w'
+                    'https://youtu.be/JZK1MZwUyUU','https://youtu.be/CyXLhHQS3KY']
